2c497800402c5f4938539d57dfb9f25d87fd916f
Vasishta Balla <vasishta.balla@gmail.com>
2021-05-02 14:14:39 +0530

    initial push

---
+++ src/test/java/dev/vasishta/jpa/hibernate/HibernateJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vasishta.jpa.hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
